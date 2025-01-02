45959964d1c1b92ed42db165bb9f2c3745679497
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2025-01-02 16:12:50 -0600

    FISH-10228: improve method implementation

--- appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ResourceHandler.java
+++ appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ResourceHandler.java
@@ -303,19 +303,19 @@ public class ResourceHandler extends AbstractResourceHandler {
 
     /**
      * Method to verify any other Environment Properties that contain same field name and return value if not empty
-     * @param fieldName field to search
+     *
+     * @param fieldName   field to search
      * @param descriptors available descriptors for EJB
      * @return String value
      */
-    public String searchValueIfAvailable(String fieldName, EnvironmentProperty[] descriptors) {
-        Optional<EnvironmentProperty> notEmptyValueOptional = Arrays.stream(descriptors)
-                .filter(d -> d.getDisplayName().equalsIgnoreCase(fieldName) || 
+    private String searchValueIfAvailable(String fieldName, EnvironmentProperty[] descriptors) {
+        return Arrays.stream(descriptors)
+                .filter(d -> d.getDisplayName().equalsIgnoreCase(fieldName) ||
                         fieldName.toLowerCase().contains(d.getDisplayName().toLowerCase()))
-                .filter(EnvironmentProperty::hasAValue).findFirst();
-        if(notEmptyValueOptional.isPresent()) {
-            return notEmptyValueOptional.get().getValue();
-        }
-        return "";
+                .filter(EnvironmentProperty::hasAValue)
+                .findFirst()
+                .map(EnvironmentProperty::getValue)
+                .orElse("");
     }
 
     private EnvironmentProperty[] getDescriptors(Class resourceType,
