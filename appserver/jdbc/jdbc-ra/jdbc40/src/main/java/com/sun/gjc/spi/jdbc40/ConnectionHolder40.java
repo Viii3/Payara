0a0c1d28cb2176c350b2198e5c33090927cfc81e
Kalin Chan <kalin.chan@payara.fish>
2025-04-02 14:37:27 +0100

    FISH-9749 Move skip to method

--- appserver/jdbc/jdbc-ra/jdbc40/src/main/java/com/sun/gjc/spi/jdbc40/ConnectionHolder40.java
+++ appserver/jdbc/jdbc-ra/jdbc40/src/main/java/com/sun/gjc/spi/jdbc40/ConnectionHolder40.java
@@ -107,7 +107,7 @@ public class ConnectionHolder40 extends ConnectionHolder {
      */
     protected void init() {
         try {
-            if (!skipClientInfoValidation && isSupportClientInfo()) {
+            if (isSupportClientInfo()) {
                 defaultClientInfo = getClientInfo();
             }
         } catch (Exception e) {
@@ -405,6 +405,9 @@ public class ConnectionHolder40 extends ConnectionHolder {
      * @since 1.6
      */
     private boolean isSupportClientInfo() {
+        if (skipClientInfoValidation) {
+            return false;
+        }
         Boolean isSupportClientInfo = getManagedConnection().isClientInfoSupported();
         if (isSupportClientInfo != null) {
             return isSupportClientInfo;
