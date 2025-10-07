f824375f89a1dfb53d77a971ea25ba4b8c036c57
Andrew Pielage <pandrex247@hotmail.com>
2025-10-07 10:00:04 +0100

    Correct Docker image name for Enterprise
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/extras/docker-images/tests/src/test/java/fish/payara/jcache/JCacheRestTest.java
+++ appserver/extras/docker-images/tests/src/test/java/fish/payara/jcache/JCacheRestTest.java
@@ -74,7 +74,7 @@ public class JCacheRestTest {
         network = Network.newNetwork();
         System.out.println("Created network with ID: " + network.getId());
 
-        DockerImageName payaraImg = DockerImageName.parse("payara/micro");
+        DockerImageName payaraImg = DockerImageName.parse("nexus.dev.payara.fish:5000/payara/micro");
         System.out.println("\n=== Starting Payara Micro Containers ===");
 
         for (int instanceIndex = 0; instanceIndex < 3; instanceIndex++) {
