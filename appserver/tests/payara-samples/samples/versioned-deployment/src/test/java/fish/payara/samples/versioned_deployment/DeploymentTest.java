f749b6b94cb289ad413694a41c89019dc66cf20a
Kalin Chan <73829904+kalinchan@users.noreply.github.com>
2025-11-03 13:09:34 +0000

    FISH-11683 EE8 Changes

--- appserver/tests/payara-samples/samples/versioned-deployment/src/test/java/fish/payara/samples/versioned_deployment/DeploymentTest.java
+++ appserver/tests/payara-samples/samples/versioned-deployment/src/test/java/fish/payara/samples/versioned_deployment/DeploymentTest.java
@@ -39,9 +39,9 @@
  */
 package fish.payara.samples.versioned_deployment;
 
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Response;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import org.jboss.arquillian.container.test.api.Deployer;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
